681d83cfe1eb29aa9f95aba55d5722e3c51cc9fd
Epaphradito <paphra.me@gmail.com>
2024-12-13 10:07:24 +0300

    Made Changes

--- resources/views/layouts/commonMaster.blade.php
+++ resources/views/layouts/commonMaster.blade.php
@@ -40,6 +40,19 @@
     <!-- Include Scripts -->
     @include('layouts/sections/scripts')
 
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-DYJJFJ881E"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-DYJJFJ881E');
+    </script>
+
 </body>
 
 </html>
